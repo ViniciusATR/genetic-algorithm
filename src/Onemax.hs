1dd99d6626ccbe87de7c77330df10a175b86fe5c
Vinicius Resende <viniatresnde@gmail.com>
2021-02-08 21:59:37 -0300

    Cria primeiras funções do algoritmo genético

--- src/Onemax.hs
+++ src/Onemax.hs
@@ -3,11 +3,52 @@ module Onemax
       cost,
       generateSolution,
       modifySolution,
-      mixSolutions,
-      selectSolution
+      -- mixSolutions,
+      -- selectSolution
     ) where
+
+import System.Random
+import Control.Monad.State.Lazy
+
 
 type Solution = [Bool]
 
-cost :: Solution -> Integer
-cost s = length (filter (\x -> x) a)
+
+cost :: Solution -> Int
+cost s = length (filter id s)
+
+
+randomState :: (StdGen -> (a, StdGen)) -> State StdGen a
+randomState randonFunction = do
+    s <- get
+    let (a, s') = randonFunction s
+    put s'
+    return a
+
+
+randomBool :: State StdGen Bool
+randomBool = randomState $ randomR (True, False)
+
+
+randomProbability :: State StdGen Double
+randomProbability = randomState $ randomR (0.0::Double, 1.0::Double)
+
+
+generateSolution :: Int -> State StdGen Solution
+generateSolution n = replicateM n randomBool
+
+
+flipBoolean :: Double -> Bool -> State StdGen Bool
+flipBoolean prob b = do
+    rand <- randomProbability
+    return $ if rand < prob then b else not b
+
+
+modifySolution :: Double -> Double -> Solution -> State StdGen Solution
+modifySolution probM indProb s = do
+    rand <- randomProbability
+    if rand < probM then
+      mapM (flipBoolean indProb) s
+    else
+      return s
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,18 +3,34 @@ module Main where
 import Lib
 import Onemax
 
+import System.Random
+import Control.Monad.State.Lazy
 import Test.Tasty
 import Test.Tasty.HUnit
+
+gen = mkStdGen 120
 
 main :: IO ()
 main = do
   defaultMain onemaxTests
 
 
-onemaxTests = (testGroup "Testes do Onemax" [ testCost testGenerator testSolutionModifier testSolutionMixer testSolutionSelector ])
+onemaxTests = (testGroup "Testes do Onemax" [ testCost , testGenerator, testSolutionModifier])
+-- testSolutionMixer testSolutionSelector ])
 
 testCost = testGroup "Testes de função de custo"
            [
-            testCase "Simple Solution" (assertEqual "Simple Solution" 5 (cost [True, True, True, False, True, False, True])),
-            testCase "All False" (assertEqual "All False" 0 (cost [False, False, False]))
+            testCase "Solução simples" (assertEqual "Solução simples" 5 (cost [True, True, True, False, True, False, True])),
+            testCase "Tudo falso" (assertEqual "Tudo falso" 0 (cost [False, False, False]))
            ]
+
+
+testGenerator = testGroup "Testes de função geradora"
+                [
+                 testCase "Solução de tamanho 5" (assertEqual "Tamanho 5" [True,True,False,False,True] (evalState (generateSolution 5) gen) )
+                ]
+
+testSolutionModifier = testGroup "Testes de modificador de soluções"
+                       [
+                        testCase "Modificar solução" (assertEqual "modificar Solução" [False, True, False, True] (evalState (modifySolution 0.9 0.2 [True, False, True, False] ) gen))
+                       ]
