9fb67b44a70ed64c75d623475b5d26cf9278da60
Vinicius Resende <viniatresnde@gmail.com>
2021-02-18 00:29:50 -0300

    Cria funções de seleção de soluções e de mistura de soluções

--- src/Onemax.hs
+++ src/Onemax.hs
@@ -1,18 +1,25 @@
 module Onemax
     ( Solution,
+      Configuration (..),
       cost,
       generateSolution,
       modifySolution,
-      -- mixSolutions,
+      mixSolutions,
       -- selectSolution
     ) where
 
+import Data.List (sortBy)
 import System.Random
 import Control.Monad.State.Lazy
 
-
+data Configuration = Configuration { solutionSize :: Int,
+                                     mutationProbability :: Double,
+                                     solutionModProbability :: Double,
+                                     solutionSetSize :: Int,
+                                     solutionMixProbability :: Double,
+                                     cutoff :: Int -- number of solutions to take from population in selection
+                                   }
 type Solution = [Bool]
-
 
 cost :: Solution -> Int
 cost s = length (filter id s)
@@ -34,21 +41,51 @@ randomProbability :: State StdGen Double
 randomProbability = randomState $ randomR (0.0::Double, 1.0::Double)
 
 
-generateSolution :: Int -> State StdGen Solution
-generateSolution n = replicateM n randomBool
+randomInt :: Int -> Int -> State StdGen Int
+randomInt min max = randomState $ randomR (min::Int, max::Int)
 
 
-flipBoolean :: Double -> Bool -> State StdGen Bool
-flipBoolean prob b = do
+generateSolution :: Configuration -> State StdGen Solution
+generateSolution c = replicateM n randomBool
+  where n = solutionSize c
+
+
+flipBoolean :: Configuration -> Bool -> State StdGen Bool
+flipBoolean c b = do
     rand <- randomProbability
-    return $ if rand < prob then b else not b
+    return $ if rand < prob then not b else b
+      where prob = mutationProbability c
 
 
-modifySolution :: Double -> Double -> Solution -> State StdGen Solution
-modifySolution probM indProb s = do
+modifySolution :: Configuration -> Solution -> State StdGen Solution
+modifySolution c s = do
     rand <- randomProbability
     if rand < probM then
-      mapM (flipBoolean indProb) s
+      mapM (flipBoolean c) s
     else
       return s
+    where probM = solutionModProbability c
 
+
+mixSolutions :: Configuration -> Solution -> Solution -> State StdGen Solution
+mixSolutions c s1 s2 = do
+    rand <- randomProbability
+    if rand < probMix then do
+        position <- randomInt 0 (size - 1)
+        let firstSection = take (position + 1) s1
+        let secondSection = drop (position + 1) s2
+        return $ firstSection ++ secondSection
+    else do
+      chooseRand <- randomBool
+      return $ if chooseRand then s1 else s2
+    where
+      probMix = solutionMixProbability c
+      size = solutionSize c
+
+
+selectSolution :: Configuration -> [Solution] -> State StdGen Solution
+selectSolution c set = do
+    let bestCandidates = take cut $ sortBy (\a b -> compare (cost a) (cost b)) set
+    randIndx <- randomInt 0 (cut - 1)
+    return $ bestCandidates!!randIndx
+      where cut = cutoff c
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Lib
 import Onemax
 
 import System.Random
@@ -9,13 +8,14 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 gen = mkStdGen 120
+conf = Configuration 4 0.99 0.99 6 0.99
 
 main :: IO ()
 main = do
   defaultMain onemaxTests
 
 
-onemaxTests = (testGroup "Testes do Onemax" [ testCost , testGenerator, testSolutionModifier])
+onemaxTests = testGroup "Testes do Onemax" [ testCost , testGenerator, testSolutionModifier]
 -- testSolutionMixer testSolutionSelector ])
 
 testCost = testGroup "Testes de função de custo"
@@ -27,10 +27,15 @@ testCost = testGroup "Testes de função de custo"
 
 testGenerator = testGroup "Testes de função geradora"
                 [
-                 testCase "Solução de tamanho 5" (assertEqual "Tamanho 5" [True,True,False,False,True] (evalState (generateSolution 5) gen) )
+                 testCase "Solução de tamanho 4" (assertEqual "Tamanho 4" [True,True,False,False] (evalState (generateSolution conf) gen) )
                 ]
 
 testSolutionModifier = testGroup "Testes de modificador de soluções"
                        [
-                        testCase "Modificar solução" (assertEqual "modificar Solução" [False, True, False, True] (evalState (modifySolution 0.9 0.2 [True, False, True, False] ) gen))
+                        testCase "Modificar solução" (assertEqual "modificar Solução" [False, True, False, True] (evalState (modifySolution conf [True, False, True, False] ) gen))
                        ]
+
+testSlutionMixer = testGroup "Teste de função de mistura de soluções"
+                   [
+                    testCase "Mistura ocorre" (assertEqual "mistura ocorre" [True, True, True, False] (evalState (mixSolutions conf [True, True, True, True] [False, False, False, False]) gen))
+                   ]
