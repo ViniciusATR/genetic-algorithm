a32c3e19bc8d241a139e44974344cffb0c611c2b
Vinicius Resende <viniatresnde@gmail.com>
2021-06-08 18:48:59 -0300

    altera pso para usar custo de rastrigin

--- src/Pso.hs
+++ src/Pso.hs
@@ -2,12 +2,14 @@ module Pso (
 
   ) where
 
+import Data.List (minimumBy)
 import System.Random
 import RandomTools
 import Control.Monad.State.Lazy
+import Utils (rastriginCost)
 
 --           CurrPosition   BestPosition CurrVelocity
-type Solution = (Double, Double, Double)
+type Solution = ([Double], [Double], [Double])
 
 
 data Configuration = Configuration {
@@ -31,58 +33,64 @@ cost x = (1/(1 + x)) * sin(2*pi*x)
 
 generateSolution :: Configuration -> State StdGen Solution
 generateSolution conf = do
-  x <- randomDouble inf sup
-  vx <- randomDouble (-maxv) maxv
-  return (x, x, vx)
+  pos <- replicateM n $ randomDouble inf sup
+  vel <- replicateM n $ randomDouble (-maxv) maxv
+  return (pos, pos, vel)
     where
+      n = solutionSize conf
       inf = infLimit conf
       sup = supLimit conf
       maxv = maxRateOfChange conf
 
 
-validateRoC :: Double -> Double -> Double
-validateRoC max roc
+validateRoC' :: Double -> Double -> Double
+validateRoC' max roc
   | roc > max = max
   | roc < -max = -max
   | otherwise = roc
 
 
--- bx  = melhor posição da particula
--- cx = posição atual da particula
--- cvx = velocidade atual da particula
--- gbx = melhor posição global
-modifySolution :: Configuration -> Double -> Solution -> State StdGen Solution
-modifySolution conf gbx (cx, bx, cv) = do
+validateRoC :: Double -> [Double] -> [Double]
+validateRoC max = map (validateRoC' max)
+
+-- b  = melhor posição da particula
+-- c = posição atual da particula
+-- cv = velocidade atual da particula
+-- gb = melhor posição global
+modifySolution :: Configuration -> [Double] -> Solution -> State StdGen Solution
+modifySolution conf gb (c, b, cv) = do
   r1 <- randomProbability
   r2 <- randomProbability
-  let nv = validateRoC maxv $ cv + gb * r1 * (gbx - cx) + lb * r2 * (bx - cx)
-  let nx = cx + nv
-  let nbx = if cost nx > cost bx then nx else bx
-  return (nx, nbx, nv)
+  let globalcoeff = zipWith (\x y -> gbias * r1 * (y - x)) c gb
+  let localcoeff = zipWith (\x y -> lbias * r2 * (y - x)) c b
+  let nv = validateRoC maxv $ zipWith3 (\a b c -> a + b + c) cv globalcoeff localcoeff
+  let np = zipWith (+) c nv
+  let nb = if rastriginCost np > rastriginCost b then np else b
+  return (np, nb, nv)
     where
       inf = infLimit conf
       sup = supLimit conf
-      gb = globalBias conf
-      lb = localBias conf
+      gbias = globalBias conf
+      lbias = localBias conf
       maxv = maxRateOfChange conf
 
 
-getBestPosition :: [Solution] -> Double
-getBestPosition = foldr (\(_,x,_) y -> if cost x > cost y then x else y) 0
+getBestPosition :: [Solution] -> Solution
+getBestPosition = minimumBy (\(a,_,_) (b,_,_) -> compare (rastriginCost a) (rastriginCost b))
 
 
-search :: Configuration -> Double
+search :: Configuration -> [Double]
 search conf = evalState (search' conf initSet initBest 0) initGen
   where
     initGen = mkStdGen $ randomSeed conf
     initSet = evalState (replicateM setSize (generateSolution conf)) initGen
     setSize = solutionSetSize conf
     maxIter = maxIterations conf
-    initBest = getBestPosition initSet
-    search' :: Configuration -> [Solution] -> Double -> Int -> State StdGen Double
+    (initBest,_,_) = getBestPosition initSet
+    search' :: Configuration -> [Solution] -> [Double] -> Int -> State StdGen [Double]
     search' conf currSet currBest currIter
       | maxIter <= currIter = return currBest
       | otherwise = do
         nextSet <- mapM (modifySolution conf currBest) currSet
-        let nextBest = getBestPosition nextSet
+        let (nextBest,_,_) = getBestPosition nextSet
         search' conf nextSet nextBest (currIter + 1)
